e84a0b09d84f984f85fc589d4aaac550839d67d2
Farha Alfarsy <fa.alfarsy@gmail.com>
2021-08-21 12:05:43 +0100

    Started Google Drive API so fiels can be uploaded onto Goodgle Drive instead being saved locally

---
+++ googledriveapi.py
@@ -0,0 +1,19 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+g_auth = GoogleAuth()
+g_auth.LocalWebserverAuth()  # starts browser and asks for authentication
+drive = GoogleDrive(g_auth)  # create a google drive object to handle file, used to list and create file
+
+# view files/folders in drive
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+for file in file_list:
+    print('Title: %s, ID: %s' % (file['title'], file[id]))
+    # get folder ID
+    if file['title'] == "To Share":
+        fileID = file['id']
+
+file1 = drive.CreateFile({"mimeType": "text/csv", "parents": [{"kind": "drive#fileLink", "id": fileID}]})
+file1.SetContentFile("small_file.csv")
+file1.Upload()
+print('Created file %s with mimeType %s' %(file1['title'], file1['mimeType']))
